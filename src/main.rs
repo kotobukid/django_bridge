b7681c81ed6063388d5403381d81cfec2de53a56
kotobukid <kotobuki@daqua.jp>
2025-02-10 12:51:08 +0900

    sqlxでpostgresに接続する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:postgres@192.168.33.10:5432/postgres")
+        .await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
 }
