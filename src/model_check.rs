19803fbcd58c315ef1c82a7c9b588c0ac81239e5
kotobukid <kotobuki@daqua.jp>
2025-02-10 14:57:13 +0900

    最小規模のPythonコード解析

--- src/model_check.rs
+++ src/model_check.rs
@@ -2,7 +2,10 @@ use rustpython_parser::{Parse, ast};
 use rustpython_parser::{lexer::lex, Mode};
 
 fn main() {
-    let python_source = "print('Hello world')";
+    let python_source = r#"
+class Card(models.Model):
+    name = models.CharField(max_length=256)
+    "#;
     // let python_statements = ast::Suite::parse(python_source).unwrap();  // statements
     // let python_expr = ast::Expr::parse(python_source).unwrap();  // or expr
 
