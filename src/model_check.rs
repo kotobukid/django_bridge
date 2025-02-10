f27e23ac849e30b6bb4da6590ee8f42f7cc7a316
kotobukid <kotobuki@daqua.jp>
2025-02-10 15:04:20 +0900

    フィールド名の取得

--- src/model_check.rs
+++ src/model_check.rs
@@ -1,16 +1,55 @@
-use rustpython_parser::{Parse, ast};
-use rustpython_parser::{lexer::lex, Mode};
+use rustpython_parser_core::mode::Mode;
+use rustpython_parser::lexer::lex;
+use rustpython_parser::Tok;
 
 fn main() {
     let python_source = r#"
 class Card(models.Model):
     name = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
     "#;
-    // let python_statements = ast::Suite::parse(python_source).unwrap();  // statements
-    // let python_expr = ast::Expr::parse(python_source).unwrap();  // or expr
 
-    let tokens: Vec<_> = lex(python_source, Mode::Module).collect();
-    // assert!(tokens.all(|t| t.is_ok()));
+    let tokens = lex(python_source, Mode::Module);
 
-    println!("{:?}", tokens);
+    let mut class_name = String::new();
+    let mut fields = vec![];
+
+    let mut tokens_iter = tokens.peekable();
+    while let Some(Ok((tok, _))) = tokens_iter.next() {
+        match tok {
+            Tok::Class => {
+                // 次のトークンがクラス名
+                if let Some(Ok((Tok::Name { name }, _))) = tokens_iter.next() {
+                    class_name = name;
+                }
+            }
+            Tok::Name { name } => {
+                // フィールド名の場合
+                if let Some(Ok((Tok::Equal, _))) = tokens_iter.peek() {
+                    let field_name = name.clone();
+                    tokens_iter.next(); // "=" を消費
+
+                    // 次のトークンがフィールド型（例: models.CharField）
+                    if let Some(Ok((Tok::Name { name: type_name }, _))) = tokens_iter.next() {
+                        if type_name == "models" {
+                            // `.`をスキップ
+                            tokens_iter.next();
+                            if let Some(Ok((Tok::Name { name: field_type }, _))) =
+                                tokens_iter.next()
+                            {
+                                fields.push((field_name, field_type));
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    println!("Class Name: {}", class_name);
+    println!("Fields:");
+    for (field_name, field_type) in fields {
+        println!("  {}: {}", field_name, field_type);
+    }
 }
