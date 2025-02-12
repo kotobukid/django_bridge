f6d8c0939f5a2c43fc6d31e218c8eb8eb1eb62ec
kotobukid <kotobuki@daqua.jp>
2025-02-12 14:04:33 +0900

    `Card構造体の導入とクエリ結果の型変換を実装`
    `django_models::CardDb`を元に`Card`構造体を導入し、`From`トレイトで変換を実装しました。これにより、SQLのクエリ結果を`Card`型に変換しやすくし、コードの可読性と拡張性を向上させました。

--- sqlm/src/main.rs
+++ sqlm/src/main.rs
@@ -1,15 +1,19 @@
 mod gen;
 
-use std::fmt::{write, Display, Formatter};
-use std::fs;
-use sqlx::postgres::{PgPool, PgPoolOptions};
-use sqlx::FromRow;
+use sqlx::postgres::PgPoolOptions;
+use std::fmt::{Display, Formatter};
 use tokio;
-use chrono;
 
 use gen::django_models;
 
+#[derive(Debug)]
 pub struct Card(pub django_models::CardDb);
+
+impl From<django_models::CardDb> for Card {
+    fn from(db: django_models::CardDb) -> Self {
+        Self(db)
+    }
+}
 
 impl Display for Card {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -18,7 +22,6 @@ impl Display for Card {
     }
 }
 
-
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     let pool = PgPoolOptions::new()
@@ -26,14 +29,15 @@ async fn main() -> Result<(), sqlx::Error> {
         .connect("postgres://postgres:postgres@192.168.33.10:5432/postgres")
         .await?;
 
-    let cards: Vec<django_models::CardDb> = sqlx::query_as::<_, django_models::CardDb>("SELECT * FROM wix_card")
-        .fetch_all(&pool)
-        .await?;
+    let cards: Vec<Card> =
+        sqlx::query_as::<_, django_models::CardDb>("SELECT * FROM wix_card")
+            .fetch_all(&pool)
+            .await?
+            .into_iter().map(|row| Card(row)).collect();
 
     for card in cards {
         println!("{:?}", card);
     }
-
 
     // assert_eq!(row.0, 150);
 
@@ -44,16 +48,5 @@ async fn main() -> Result<(), sqlx::Error> {
     // let python_source = fs::read_to_string(file_path)?;
     // println!("{}", python_source);
 
-
-
-    let card = Card(django_models::CardDb {
-        id: 1,
-        name: "Taro".into(),
-        created_at: chrono::Utc::now(),
-        bool1: true,
-        option1: Some("jiro".into())
-    });
-
-    println!("{}", card);
     Ok(())
 }
