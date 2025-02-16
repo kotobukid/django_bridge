7e20a00013065164d6610d3482f3e264c00dbbef
kotobukid <kotobuki@daqua.jp>
2025-02-17 02:24:47 +0900

    静的ファイルの設定を修正
    STATIC_ROOTを設定し、STATICFILES_DIRSに正しいディレクトリを追加。また、urlpatternsに静的ファイルのURLパターンを設定して、Djangoサーバーで静的ファイルを配信可能に修正。

--- table_definition/table_definition/settings.py
+++ table_definition/table_definition/settings.py
@@ -123,9 +123,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 # manage.py run_with_custom_adminで起動時、adminサイトのルートアドレスを外部から注入している
 # print(CUSTOM_ADMIN_ROOT)
--- table_definition/table_definition/urls.py
+++ table_definition/table_definition/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path(settings.CUSTOM_ADMIN_ROOT, admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
