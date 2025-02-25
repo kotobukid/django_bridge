69a34cca15483852df5a6dfeebdf7759f6bc24aa
kotobukid <kotobuki@daqua.jp>
2025-02-25 15:11:04 +0900

    APIエンドポイントを追加しカードリストをJSON形式で取得可能に
    新しいエンドポイント`/api/card_list.json`を追加しました。このエンドポイントでは、カードリストをJSON形式で返すように実装し、`OnlyCardName`モデルからデータをシリアライズします。既存の文字列形式のリスト取得機能も保持しています。

--- webapp/src/routers/card_router.rs
+++ webapp/src/routers/card_router.rs
@@ -1,11 +1,14 @@
-use std::sync::Arc;
-use axum::extract::State;
+use crate::models::{OnlyCardName, OnlyCardNameRepository};
 use crate::state::AppState;
+use axum::extract::State;
+use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
+use axum::Json;
 use axum::Router;
+use serde::Serialize;
 use sqlx::{Pool, Postgres};
-use crate::models::OnlyCardNameRepository;
+use std::sync::Arc;
 
 #[derive(Clone)]
 struct RouterState {
@@ -14,17 +17,42 @@ struct RouterState {
 
 pub fn create_card_router(pool: Arc<Pool<Postgres>>) -> Router<AppState> {
     let only_card_name_repo = OnlyCardNameRepository::new(pool);
-    let state = RouterState { card_repo: only_card_name_repo };
+    let state = RouterState {
+        card_repo: only_card_name_repo,
+    };
 
-    Router::new().route("/", get(card_list))
+    Router::new()
+        .route("/", get(card_list))
+        .route("/api/card_list.json", get(card_list_json))
         .with_state(state)
 }
 
 async fn card_list(State(state): State<RouterState>) -> impl IntoResponse {
-    state.card_repo
+    let cards = state.card_repo.get_all().await;
+
+    match cards {
+        Ok(card_list) => card_list
+            .into_iter()
+            .map(|card| card.name)  // OnlyCardNameから直接nameフィールドを取得
+            .collect::<Vec<String>>()
+            .join("\n"),
+        Err(_) => "エラーが発生しました".to_string()
+    }
+}
+
+#[derive(Serialize)]
+struct CardListJson {
+    cards: Vec<OnlyCardName>,
+}
+async fn card_list_json(
+    State(state): State<RouterState>,
+) -> Result<Json<CardListJson>, StatusCode> {
+    let cards = state
+        .card_repo
         .get_all()
         .await
-        .into_iter()
-        .map(|card| card.to_string())
-        .collect::<Vec<String>>().join("\n")
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let res: CardListJson = CardListJson { cards };
+    Ok(Json(res))
 }
