44e147a173316c1d52fc98a3a861d9303c2a533c
kotobukid <kotobuki@daqua.jp>
2025-03-05 19:52:11 +0900

    wasmファイルのロード関連で難儀中
    pkgを
    front/static/pkg
    public/pkg
    のように重複配置することで暫定的に呼び出し成功となったが、追って検証する

--- front/pages/card.vue
+++ front/pages/card.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+
+// 表示用のメッセージ
+const message = ref('default');
+
+// Wasm 実行
+const runWasm = async () => {
+  try {
+    // Wasmパッケージを動的にインポート
+    const { default: init, greet } = await import('/static/pkg/datapack.js');
+
+    // 初期化を呼び出し (WasmファイルのURLを暗黙的に指定)
+    await init('/pkg/datapack_bg.wasm');
+
+
+    // Wasm関数を実行 (例: greet)
+    message.value = greet('Nuxt');
+  } catch (err) {
+    console.error('Failed to load Wasm:', err);
+    message.value = 'Error loading Wasm';
+  }
+};
+
+onMounted(runWasm);
 
 </script>
 
 <template lang="pug">
+  span {{ message }}
   .frame
     NavBar
 </template>
