c0ae79bd0cff8c75719e362b002447dfe7de7aa4
kotobukid <kotobuki@daqua.jp>
2025-03-14 22:38:15 +0900

    番号がズレたFeatureに対応

--- webapp/sample.sql
+++ webapp/sample.sql
@@ -12,13 +12,13 @@ where code LIKE 'WX24-P2%'
 order by feature_bits1 ASC;
 
 
--- BOUNCE(bits1, 22)
+-- ENER(bits1, 20)
 select id, name, code, feature_bits1 as f1, feature_bits2 as f2, skill_text, burst_text
 from wix_card
 where (feature_bits1 & (1 << 20)) <> 0
 order by feature_bits1 ASC;
 
--- BOUNCE(22) & SALVAGE(24)
+-- DECK BOUNCE(24) & POWER DOWN(22)
 select id, name, code, feature_bits1 as f1, feature_bits2 as f2, skill_text, burst_text
 from wix_card
 where (feature_bits1 & (1 << 22)) <> 0
