05b5fc27b4b9c5d1bff8796b45a2d6c063acf052
kotobukid <kotobuki@daqua.jp>
2025-06-07 01:26:33 +0900

    GithubPagesのカスタムドメイン前提の設定

---
+++ pub.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# GithubPagesへ公開するスクリプト
+
+# まずビルド
+cargo make trunk_pages
+
+# gh-pages仕様への対応
+touch ./dist-pages/.nojekyll
+cp ./wasm_front/gh-pages-404.html ./dist-pages/404.html
+
+# 一時的なブランチを作成してチェックアウト
+git checkout -b temp-deploy
+
+# ビルドファイルの追加とコミット
+git add dist-pages/ --force
+git commit -m "Update dist files with relative paths for GitHub Pages"
+
+# gh-pagesブランチへのデプロイ
+git subtree push --prefix=dist-pages github gh-pages
+
+# 元のブランチに戻る
+git checkout -
+# 一時ブランチの削除
+git branch -D temp-deploy
