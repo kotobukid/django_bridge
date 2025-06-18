2447941370083d9f47c1da31b609b388669dbcc4
kotobukid <kotobuki@daqua.jp>
2025-06-18 13:31:54 +0900

    テキスト検索ボタン押下時、入力欄のテキストがクリアされていなかった

--- wasm_front/src/components/text_search.rs
+++ wasm_front/src/components/text_search.rs
@@ -46,7 +46,7 @@ pub fn TextSearch(
                         type="text"
                         class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                         placeholder="カード名、コード、読みで検索..."
-                        value=move || search_text.get()
+                        prop:value=move || search_text.get()
                         on:input=on_input
                     />
                     <Show when=move || has_text.get()>
